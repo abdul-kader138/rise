ed8d3d5d5d7302036e4e16620082febeb4d9d1b1
abdul-kader138 <babu313136@gmail.com>
2018-11-08 18:29:49 +0600

    include Additional information for client

--- application/controllers/Clients_events.php
+++ application/controllers/Clients_events.php
@@ -1,10 +1,6 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: a.kader
- * Date: 08-Nov-18
- * Time: 3:39 PM
- */
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
 
 class Clients_events extends MY_Controller {
 
@@ -12,14 +8,39 @@ class Clients_events extends MY_Controller {
     {
         parent::__construct();
     }
-
-    //load calendar view
     function index($encrypted_event_id = "")
     {
         //$this->check_module_availability("module_event");
         $view_data['encrypted_event_id'] = $encrypted_event_id;
 
         $this->template->rander("clients_event/index", $view_data);
+    }
+
+    function calendar_events()
+    {
+          $list_data_of_events = $this->Clients_model->get_details(null)->result();
+           foreach ($list_data_of_events as $data) {
+        //check if this recurring event, generate recurring evernts based on the condition
+            $data->cycle = 0; //it's required to calculate the recurring events
+            $result[] = $this->_make_calendar_event($data); //add regular event
+
+        }
+        echo  json_encode($result);
+    }
+
+    //load calendar view
+
+    private function _make_calendar_event($data) {
+        if(isset($data->date_of_birth)){
+            $month=substr($data->date_of_birth,5,2);
+            $date = date("Y-m-d");
+            $current_month=substr($date,5,2);
+            if($current_month == $month)
+            return array(
+                "title" => $data->client_name,
+                "start" => $data->date_of_birth
+            );
+        }
     }
 
 }
--- application/views/clients_event/index.php
+++ application/views/clients_event/index.php
@@ -24,14 +24,14 @@ if (isset($client_id)) {
                 <h1><?php echo lang('client_event_calendar'); ?></h1>
             <?php } ?>
             <div class="title-button-group">
-<!--                --><?php //echo modal_anchor(get_uri("events/modal_form"), "<i class='fa fa-plus-circle'></i> " . lang('add_event'), array("class" => "btn btn-default", "title" => lang('add_event'), "data-post-client_id" => $client)); ?>
-<!--                --><?php //echo modal_anchor(get_uri("events/modal_form"), "", array("class" => "hide", "id" => "add_event_hidden", "title" => lang('add_event'), "data-post-client_id" => $client)); ?>
-<!--                --><?php //echo modal_anchor(get_uri("events/view"), "", array("class" => "hide", "id" => "show_event_hidden", "data-post-client_id" => $client, "data-post-cycle" => "0", "data-post-editable" => "1", "title" => lang('event_details'))); ?>
-<!--                --><?php //echo modal_anchor(get_uri("leaves/application_details"), "", array("class" => "hide", "data-post-id" => "", "id" => "show_leave_hidden")); ?>
+                <!--                --><?php //echo modal_anchor(get_uri("events/modal_form"), "<i class='fa fa-plus-circle'></i> " . lang('add_event'), array("class" => "btn btn-default", "title" => lang('add_event'), "data-post-client_id" => $client)); ?>
+                <!--                --><?php //echo modal_anchor(get_uri("events/modal_form"), "", array("class" => "hide", "id" => "add_event_hidden", "title" => lang('add_event'), "data-post-client_id" => $client)); ?>
+                <!--                --><?php //echo modal_anchor(get_uri("events/view"), "", array("class" => "hide", "id" => "show_event_hidden", "data-post-client_id" => $client, "data-post-cycle" => "0", "data-post-editable" => "1", "title" => lang('event_details'))); ?>
+                <!--                --><?php //echo modal_anchor(get_uri("leaves/application_details"), "", array("class" => "hide", "data-post-id" => "", "id" => "show_leave_hidden")); ?>
             </div>
         </div>
         <div class="panel-body">
-            <div id="event-calendar"></div>
+            <div id="event-calendar1"></div>
         </div>
     </div>
 </div>
@@ -39,68 +39,20 @@ if (isset($client_id)) {
 <script type="text/javascript">
     $(document).ready(function () {
 
-        $("#event-calendar").fullCalendar({
-            lang: AppLanugage.locale,
+        $("#event-calendar1").fullCalendar({
+//            lang: AppLanugage.locale,
             header: {
                 left: 'prev,next today',
-                center: 'title',
-                right: 'month,agendaWeek,agendaDay'
+                center: 'title'
+//                right: 'month,agendaWeek,agendaDay'
             },
-            events: "<?php echo_uri("events/calendar_events/" . $client); ?>",
-            dayClick: function (date, jsEvent, view) {
-                $("#add_event_hidden").attr("data-post-start_date", date.format("YYYY-MM-DD"));
-                var startTime = date.format("HH:mm:ss");
-                if (startTime === "00:00:00") {
-                    startTime = "";
-                }
-                $("#add_event_hidden").attr("data-post-start_time", startTime);
-                var endDate = date.add(1, 'hours');
-
-                $("#add_event_hidden").attr("data-post-end_date", endDate.format("YYYY-MM-DD"));
-                var endTime = "";
-                if (startTime != "") {
-                    endTime = endDate.format("HH:mm:ss");
-                }
-
-                $("#add_event_hidden").attr("data-post-end_time", endTime);
-                $("#add_event_hidden").trigger("click");
-            },
-            eventClick: function (calEvent, jsEvent, view) {
-                $("#show_event_hidden").attr("data-post-id", calEvent.encrypted_event_id);
-                $("#show_event_hidden").attr("data-post-cycle", calEvent.cycle);
-                $("#show_leave_hidden").attr("data-post-id", calEvent.encrypted_event_id);
-
-                if (calEvent.event_type === "event") {
-                    $("#show_event_hidden").trigger("click");
-                } else {
-                    $("#show_leave_hidden").trigger("click");
-                }
-            },
-            eventRender: function (event, element) {
-                if (event.icon) {
-                    element.find(".fc-title").prepend("<i class='fa " + event.icon + "'></i> ");
-                }
-            },
-            firstDay: AppHelper.settings.firstDayOfWeek
+            events: '<?php base_url(); ?>clients_events/calendar_events/'
+//            events: [{"title" : "Hello","start" :"2018-11-07"}]
 
         });
 
-        var client = "<?php echo $client; ?>";
-        if (client) {
             setTimeout(function () {
-                $('#event-calendar').fullCalendar('today');
+                $('#event-calendar1').fullCalendar('today');
             });
-        }
-
-
-        //autoload the event popover
-        var encrypted_event_id = "<?php echo isset($encrypted_event_id) ? $encrypted_event_id : ''; ?>";
-
-        if (encrypted_event_id) {
-            $("#show_event_hidden").attr("data-post-id", encrypted_event_id);
-            $("#show_event_hidden").trigger("click");
-        }
-
-
     });
 </script>
